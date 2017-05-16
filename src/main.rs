43591c3422787ac25de5d3faa8149b6e5d3ec7da
Alaina Herkelman <Alaina Herkelman>
2017-05-15 20:22:03 -0400

    hi

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,120 @@
-extern crate piston;
+extern crate piston_window;
+extern crate ai_behavior;
+extern crate sprite;
+extern crate find_folder;
+
 extern crate graphics;
-extern crate glutin_window;
-extern crate opengl_graphics;
 
-use piston::window::WindowSettings;
-use piston::event_loop::*;
-use piston::input::*;
-use glutin_window::GlutinWindow as Window;
-use opengl_graphics::{ GlGraphics, OpenGL };
 
-pub struct App {
-    gl: GlGraphics, // OpenGL drawing backend.
-    rotation: f64   // Rotation for the square.
-}
 
-impl App {
-    fn render(&mut self, args: &RenderArgs) {
-        use graphics::*;
+use graphics::types::SourceRectangle;
+use std::rc::Rc;
 
-        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 
-        let square = rectangle::square(0.0, 0.0, 50.0);
-        let rotation = self.rotation;
-        let (x, y) = ((args.width / 2) as f64,
-                      (args.height / 2) as f64);
-
-        self.gl.draw(args.viewport(), |c, gl| {
-            // Clear the screen.
-            clear(GREEN, gl);
-
-            let transform = c.transform.trans(x, y)
-                                       .rot_rad(rotation)
-                                       .trans(-25.0, -25.0);
-
-            // Draw a box rotating around the middle of the screen.
-            rectangle(RED, square, transform, gl);
-        });
-    }
-
-    fn update(&mut self, args: &UpdateArgs) {
-        // Rotate 2 radians per second.
-        self.rotation += 2.0 * args.dt;
-    }
-}
+use piston_window::*;
+use sprite::*;
+use ai_behavior::{
+    Action,
+    Sequence,
+    Wait,
+    WaitForever,
+    While,
+};
 
 fn main() {
-    // Change this to OpenGL::V2_1 if not working.
+    let (width, height) = (1000, 600);
     let opengl = OpenGL::V3_2;
-
-    // Create an Glutin window.
-    let mut window: Window = WindowSettings::new(
-            "spinning-square",
-            [200, 200]
-        )
-        .opengl(opengl)
+    let mut window: PistonWindow =
+        WindowSettings::new("piston: sprite", (width, height))
         .exit_on_esc(true)
+        .opengl(opengl)
         .build()
         .unwrap();
 
-    // Create a new game and run it.
-    let mut app = App {
-        gl: GlGraphics::new(opengl),
-        rotation: 0.0
-    };
+    let assets = find_folder::Search::ParentsThenKids(3, 3)
+        .for_folder("Images").unwrap();
+    let id;
+    let mut scene = Scene::new();
+    let tex = Rc::new(Texture::from_path(
+            &mut window.factory,
+            assets.join("cards.png"),
+            Flip::None,
+            &TextureSettings::new()
+        ).unwrap());
 
-    let mut events = Events::new(EventSettings::new());
-    while let Some(e) = events.next(&mut window) {
-        if let Some(button) = e.release_args() {
-            match button {
-                Button::Keyboard(key) => println!("Released keyboard key '{:?}'", key),
-                Button::Mouse(button) => println!("Released mouse button '{:?}'", button),
-                Button::Controller(button) => println!("Released controller button '{:?}'", button),
-            }
-        };
-        e.mouse_cursor(|x, y| {
-            //cursor = [x, y];
-            println!("Mouse moved '{} {}'", x, y);
+    let rust_logo = assets.join("background.jpg");
+    let rust_logo = Texture::from_path(
+            &mut window.factory,
+            &rust_logo,
+            Flip::None,
+            &TextureSettings::new()
+        ).unwrap();
+
+    let k:SourceRectangle=[0.0,0.0,75.0,100.0];
+    let mut sprite = Sprite::from_texture_rect(tex.clone(),k);
+    let k:SourceRectangle=[75.0,0.0,75.0,100.0];
+    let mut sprite2 = Sprite::from_texture_rect(tex.clone(),k); 
+
+
+
+    sprite.set_position(width as f64 / 2.0, height as f64 / 2.0);
+    sprite2.set_position(width as f64 / 2.0-100.0, height as f64 / 2.0-125.0);
+
+    id = scene.add_child(sprite);
+    //let id2 = scene.add_child(sprite2);
+
+
+    // Run a sequence of animations.
+    let seq = Sequence(vec![
+        Action(Ease(EaseFunction::CubicOut, Box::new(ScaleTo(2.0, 0.5, 0.5)))),
+        Action(Ease(EaseFunction::BounceOut, Box::new(MoveBy(1.0, 0.0, 100.0)))),
+        Action(Ease(EaseFunction::ElasticOut, Box::new(MoveBy(2.0, 0.0, -100.0)))),
+        Action(Ease(EaseFunction::BackInOut, Box::new(MoveBy(1.0, 0.0, -100.0)))),
+        Wait(0.5),
+        Action(Ease(EaseFunction::ExponentialInOut, Box::new(MoveBy(1.0, 0.0, 100.0)))),
+        Action(Blink(1.0, 5)),
+        While(Box::new(WaitForever), vec![
+            Action(Ease(EaseFunction::QuadraticIn, Box::new(FadeOut(1.0)))),
+            Action(Ease(EaseFunction::QuadraticOut, Box::new(FadeIn(1.0)))),
+        ]),
+    ]);
+    scene.run(id, &seq);
+
+    // This animation and the one above can run in parallel.
+    let rotate = Action(Ease(EaseFunction::ExponentialInOut,
+        Box::new(RotateTo(2.0, 360.0))));
+    scene.run(id, &rotate);
+    //scene.run(id2,&rotate);
+    println!("Press any key to pause/resume the animation!");
+    while let Some(e) = window.next() {
+        scene.event(&e);
+
+        window.draw_2d(&e, |c, g| {
+            clear([1.0, 1.0, 1.0, 1.0], g);
+            image(&rust_logo, c.transform, g);
+            scene.draw(c.transform, g);
+            sprite2.draw(c.transform,g);
         });
-        if let Some(r) = e.render_args() {
-            app.render(&r);
+        if let Some(_) = e.press_args() {
+            scene.toggle(id, &seq);
+            scene.toggle(id, &rotate);
+        }
+        if let Some(button) = e.press_args() {
+            println!("button: {:?}",button);
+            //use piston_window::Key;
+            //if button == Keyboard(Key::G)
+            //if let Some(Button::Keyboard(Key::A)) = e.press_args()
         }
 
-        if let Some(u) = e.update_args() {
-            app.update(&u);
+        // if let Some(button) = e.press_args() {
+        //     if button == Button::Mouse(MouseButton::Left) {
+        //         draw = true;
+        // }
+        if let Some(pos) = e.mouse_cursor_args() {
+            println!("{:?}",pos);
+            let (x, y) = (pos[0] as f64, pos[1] as f64);
+            sprite2.set_position(x, y);
         }
     }
 }
+ 
